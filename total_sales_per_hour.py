9c87a13dd1fd7d17c9aa7fd397a615760d6df185
Ahmadcodesss <ahmadiftikhar6789@gmail.com>
2026-08-28 18:05:01 +0500

    total sales per hour

--- total_sales_per_hour.py
+++ total_sales_per_hour.py
@@ -6,4 +6,4 @@ df = pd.read_excel("bakery.xlsx", sheet_name="Data")
 # Sum of 'total' for each hour (this is the SUMIF equivalent, repeated for all hours)
 hourly_totals = df.groupby("hour")["total"].sum().sort_index()
 
-print(hourly_totals)
+print(hourly_totals) 
